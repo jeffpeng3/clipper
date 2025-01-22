9b238e203358e1d8e10949906af3c7758feda2dc
jeff <jeffpeng2012@gmail.com>
2025-01-23 01:30:13 +0800

    feat: use arboard to support x11 and wayland

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use wl_clipboard_rs::copy::{MimeType, Options, Source};
+use anyhow::Result;
+use std::io::{self, Read, Cursor};
+use arboard::{Clipboard, ImageData};
+use image::ImageReader;
 
-
-fn main() -> Result<(), anyhow::Error>{
-    let opts = Options::new();
-    let r = opts.copy(Source::StdIn, MimeType::Autodetect);
-
-    match r {
-        Ok(r) => println!("copied {:?}", r),
-        Err(e) => println!("error: {:?}", e),
-    }
-
-    std::thread::sleep(std::time::Duration::from_millis(10));
+fn main()  -> Result<()> {
+    let mut buf: Vec<u8> = Vec::new();
+    io::stdin().read_to_end(&mut buf)?;
+    println!("Read {} bytes", buf.len());
+    let mut ctx = Clipboard::new()?;
+    let img = ImageReader::new(Cursor::new(buf)).with_guessed_format()?.decode()?.into_rgba8();
+    println!("Image: {}x{}", img.width(), img.height());
+    let (w,h) = img.dimensions();
+    let img_data = ImageData{ width: w as usize, height: h as usize, bytes: img.into_raw().into() };
+    println!("Setting image data");
+    ctx.set_image(img_data)?;
+    let r = ctx.get_image()?;
+    println!("Read image data: {}x{}", r.width, r.height);
     Ok(())
 }
